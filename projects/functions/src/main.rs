2d851c1381f3d8959bc2a96f2c061ed56f097bcd
Leo Griffiths <9199543+LeoTheGriff@users.noreply.github.com>
2025-01-14 17:53:19 +0000

    feat: add five function to demonstrate early return and update main function to use its result

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -1,8 +1,15 @@
+fn five() -> i32 {
+    // You can return early from a function by using the return keyword and specifying a value,
+    // but most functions return the last expression implicitly.
+    4 + 1
+}
+
 fn main() {
     println!("Hello, world!");
 
-    print_labelled_measurement(5, 'm');
+    let x = five();
 
+    print_labelled_measurement(x, 'm');
 }
 
 // You must declare the type of each parameter in the function signature.
