5e5c0bbe21d132899f975a657d3da87595d46fb4
Leo Griffiths <9199543+LeoTheGriff@users.noreply.github.com>
2025-01-14 17:11:07 +0000

    refactor: prefix variables with underscore to suppress unused variable warnings

--- projects/hello_cargo/src/main.rs
+++ projects/hello_cargo/src/main.rs
@@ -3,46 +3,51 @@ fn main() {
 
 
     println!("Hello, data types!");
+    // NB: Variables are prefixed _ to suppress warnings about unused variables
 
     // NUMERIC OPERATORS
     // Basic operators as expected
-    let sum = 5 + 10;
-    let difference = 95.5 - 4.3;
-    let product = 4 * 30;
+    let _sum = 5 + 10;
+    let _difference = 95.5 - 4.3;
+    let _product = 4 * 30;
 
     // Integer division truncates
-    let quotient = 56.7 / 32.2;
-    let truncated = -5 / 3; // Results in -1
+    let _quotient = 56.7 / 32.2;
+    let _truncated = -5 / 3; // Results in -1
 
     // remainder
-    let remainder = 43 % 5;
+    let _remainder = 43 % 5;
     
     // BOOLEAN OPERATORS
-    let t = true;
-    let f: bool = false; // with explicit type annotation
+    let _t = true;
+    let _f: bool = false; // with explicit type annotation
 
     // TEXT
     // Characters are single quoted and represent a Unicode Scalar Value
-    let c = 'z';
-    let z: char = 'ℤ'; // with explicit type annotation
-    let heart_eyed_cat = '😻';
+    let _c = 'z';
+    let _z: char = 'ℤ'; // with explicit type annotation
+    let _heart_eyed_cat = '😻';
 
     // Strings are double quoted and UTF-8 encoded
-    let s = "hello";
+    let _s = "hello";
 
 
     println!("Hello Compound Types!");
 
     // Tuples group a number of values with a variety of types into one compound type.
     // Tuples have a fixed length.
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let _tup: (i32, f64, u8) = (500, 6.4, 1);
 
     // Tuples are mutable and can be accessed by destructuring or index
-    let mut tup = (500, 6.4, 1);
-    println!("The value of y is: {}", tup.2);
-    tup = (500, 6.4, 2);
-    println!("The value of y is: {}", tup.2); // Accessing tuple elements by index, starting at 0
-    let (x, y, z) = tup;
-    println!("The value of y is: {}", y); // Using pattern matching to destructure a tuple
+    let mut _tup = (500, 6.4, 1);
+    println!("The value of y is: {}", _tup.2);
+    _tup = (500, 6.4, 2);
+    println!("The value of y is: {}", _tup.2); // Accessing tuple elements by index, starting at 0
+    let (_x, _y, _z) = _tup;
+    println!("The value of y is: {}", _y); // Using pattern matching to destructure a tuple
+
+    // The tuple without any values has a special name, unit.
+    let _unit = ();
+    let _unit: () = ();
 
 }
