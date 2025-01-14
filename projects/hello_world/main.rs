0aea3d61c81a1525a1b1fad342fc69a864059757
Leo Griffiths <9199543+LeoTheGriff@users.noreply.github.com>
2025-01-14 16:54:39 +0000

    feat: add numeric operations examples in main function

--- projects/hello_world/main.rs
+++ projects/hello_world/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
+
+    // NUMERIC OPERATORS
+    // Basic operators as expected
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let product = 4 * 30;
+
+    // Integer division truncates
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
+
 }
